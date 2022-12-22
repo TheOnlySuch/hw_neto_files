b4b59500f29db2b1b26918671dc6affc6e78b676
TheOnlySuch <118209545+TheOnlySuch@users.noreply.github.com>
2022-12-22 11:01:23 +0300

    Update Open_files.py

--- Open_files.py
+++ Open_files.py
@@ -19,11 +19,13 @@ def sorted_new(line):
 def write_file(file1, file2, file3):
     sorted_dict=sorted_new(line_count)
     for key in sorted_dict.keys():
-        for i, x in enumerate([file1, file2, file3]):
+        for x in [file1, file2, file3]:
             if key==x:
                 with open(x) as f:
-                    number_of_rows=sum(1 for _ in f)
+                    number_of_rows=sum(1 for g in f)
                 with open(x) as f, open('result.txt', 'a') as res:
                     res.write(f'Имя файла: {x}\nколичество строк:{number_of_rows}\n{f.read()}\n')
                     
+
+
 write_file('1.txt', '2.txt', '3.txt')
